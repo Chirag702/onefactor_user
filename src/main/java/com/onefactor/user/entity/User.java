174f5875e03d4a42542cabca328878cf3a98fc78
Chirag Agarwal <chiragagrawal629@gmail.com>
2024-11-01 21:57:21 +0530

    c

--- src/main/java/com/onefactor/user/entity/User.java
+++ src/main/java/com/onefactor/user/entity/User.java
@@ -2,6 +2,7 @@ package com.onefactor.user.entity;
 
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,7 +18,8 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+	@Column(nullable = false, unique=true)
+
 	private String email;
 	
 	private String fname;
